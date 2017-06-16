9060fa5a84021c70319520f2461e492426e230c0
Juan Rondon <juan.rondon@livn.world>
2017-06-16 12:17:56 +1000

    Changed server message

--- cache.js
+++ cache.js
@@ -3,7 +3,7 @@ const client = redis.createClient()
 
 //Incase any error pops up, log it
 client.on("error", function(err) {
-  console.log("Error " + err);
+  console.log("Error  " + err);
 })
 
 module.exports = client
